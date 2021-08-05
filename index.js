4c4f827eb9e317f14537963099735d7c4d7fe3f9
Titus Wormer <tituswormer@gmail.com>
2021-08-05 12:58:55 +0200

    Remove warning for remark 12

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 import {footnote} from 'micromark-extension-footnote'
 import {footnoteFromMarkdown, footnoteToMarkdown} from 'mdast-util-footnote'
 
-let warningIssued
-
 export default function remarkFootnotes(options) {
   const data = this.data()
-
-  // Old remark.
-  /* c8 ignore next 14 */
-  if (
-    !warningIssued &&
-    ((this.Parser &&
-      this.Parser.prototype &&
-      this.Parser.prototype.blockTokenizers) ||
-      (this.Compiler &&
-        this.Compiler.prototype &&
-        this.Compiler.prototype.visitors))
-  ) {
-    warningIssued = true
-    console.warn(
-      '[remark-footnotes] Warning: please upgrade to remark 13 to use this plugin'
-    )
-  }
 
   add('micromarkExtensions', footnote(options))
   add('fromMarkdownExtensions', footnoteFromMarkdown)
